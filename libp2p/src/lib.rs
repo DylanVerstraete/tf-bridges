ed42a1e557d085683fd83ed43b9e62f49ed2a9ab
dylanverstraete <dylan_verstraete1@hotmail.com>
2023-03-06 13:44:49 +0100

    refactor

--- libp2p/src/lib.rs
+++ libp2p/src/lib.rs
@@ -1,15 +1,15 @@
+use behaviour::{Behaviour, Event};
 use futures::prelude::*;
 use libp2p::{
     identity::Keypair,
     multiaddr::Protocol,
-    relay,
-    swarm::{Swarm, SwarmEvent},
+    swarm::{Swarm, SwarmEvent, THandlerErr},
     Multiaddr, PeerId,
 };
-use log::{debug, info};
+use log::{debug, error, info};
+use std::{error::Error, fs, path::Path};
+
 pub mod behaviour;
-use behaviour::{Behaviour, Event};
-use std::{error::Error, fs, path::Path, str::FromStr};
 
 pub struct Libp2pHost {
     pub identity: Keypair,
@@ -42,29 +42,24 @@ impl Libp2pHost {
         })
     }
 
-    pub async fn connect_to_relay(
-        mut self,
-        relay: String,
-        peer_id: &str,
-    ) -> Result<(), Box<dyn Error>> {
-        let relay_peer_id = PeerId::from_str(peer_id)?;
+    pub async fn connect_to_relay(&mut self, relay: String) -> Result<(), Box<dyn Error>> {
+        info!("connecting to relay: {}", relay);
         let relay_addr: Multiaddr = relay.parse()?;
 
-        let dest_relay_addr = relay_addr
-            .clone()
-            .with(Protocol::P2p(relay_peer_id.into()))
-            .with(Protocol::P2pCircuit);
+        let dest_relay_addr = relay_addr.clone().with(Protocol::P2pCircuit);
 
-        debug!("dest relay addr: {:?}", dest_relay_addr.clone());
-
+        // Listen on the destination relay address so other peers can find us
         self.swarm.listen_on(dest_relay_addr.clone())?;
-        wait_for_dial(&mut self, relay_peer_id).await;
-
-        info!("making reservation...");
-        wait_for_reservation(&mut self, dest_relay_addr.clone(), relay_peer_id, false).await;
-        self.swarm.dial(dest_relay_addr.clone())?;
 
         Ok(())
+    }
+
+    pub async fn run(mut self) -> Result<(), ()> {
+        loop {
+            tokio::select! {
+                swarm_event = self.swarm.select_next_some() => self.handle_swarm_event(swarm_event).await?,
+            }
+        }
     }
 
     pub fn ping(mut self, addr: String) -> Result<(), Box<dyn Error>> {
@@ -79,6 +74,43 @@ impl Libp2pHost {
         self.swarm.dial(remote)?;
         Ok(())
     }
+
+    #[allow(clippy::type_complexity)]
+    async fn handle_swarm_event(
+        &mut self,
+        event: SwarmEvent<Event, THandlerErr<Behaviour>>,
+    ) -> Result<(), ()> {
+        match event {
+            SwarmEvent::Behaviour(Event::Identify(event)) => {
+                info!("found identify event: {:?}", event);
+            }
+            SwarmEvent::NewListenAddr { address, .. } => {
+                let peer_id = self.swarm.local_peer_id().to_string();
+                info!("Listening on {:?}, {:?}", address, peer_id);
+            }
+            SwarmEvent::ConnectionEstablished {
+                peer_id, endpoint, ..
+            } => {
+                info!("Connection established {:?} on {:?}", peer_id, endpoint);
+            }
+            SwarmEvent::OutgoingConnectionError {
+                peer_id: maybe_peer_id,
+                error,
+                ..
+            } => {
+                error!(
+                    "Outgoing connection error: {:?} to peer: {:?}",
+                    error, maybe_peer_id
+                );
+            }
+            SwarmEvent::Behaviour(Event::Relay(e)) => info!("{:?}", e),
+            SwarmEvent::Behaviour(Event::Ping(_)) => {}
+            ev => {
+                debug!("other event: {:?}", ev);
+            }
+        }
+        Ok(())
+    }
 }
 
 /// Read the pre shared key file from the given ipfs directory
@@ -88,68 +120,5 @@ pub fn get_psk(path: &Path) -> std::io::Result<Option<String>> {
         Ok(text) => Ok(Some(text)),
         Err(e) if e.kind() == std::io::ErrorKind::NotFound => Ok(None),
         Err(e) => Err(e),
-    }
-}
-
-async fn wait_for_reservation(
-    client: &mut Libp2pHost,
-    client_addr: Multiaddr,
-    relay_peer_id: PeerId,
-    is_renewal: bool,
-) {
-    let mut new_listen_addr = false;
-    let mut reservation_req_accepted = false;
-    let client_addr = client_addr.with(Protocol::P2p(client.local_peer_id.into()));
-    loop {
-        match client.swarm.select_next_some().await {
-            SwarmEvent::Behaviour(Event::Relay(relay::client::Event::ReservationReqAccepted {
-                relay_peer_id: peer_id,
-                renewal,
-                ..
-            })) if relay_peer_id == peer_id && renewal == is_renewal => {
-                reservation_req_accepted = true;
-                if new_listen_addr {
-                    break;
-                }
-            }
-            SwarmEvent::NewListenAddr { address, .. } if address == client_addr => {
-                new_listen_addr = true;
-                if reservation_req_accepted {
-                    break;
-                }
-            }
-            SwarmEvent::Behaviour(Event::Ping(_)) => {}
-            SwarmEvent::ListenerClosed {
-                addresses, reason, ..
-            } => {
-                debug!("listener {:?} closed because of: {:?}", addresses, reason);
-            }
-            ev => {
-                debug!("other event: {:?}", ev);
-            }
-        }
-    }
-}
-
-async fn wait_for_dial(client: &mut Libp2pHost, remote: PeerId) -> bool {
-    loop {
-        match client.swarm.select_next_some().await {
-            SwarmEvent::Dialing(peer_id) if peer_id == remote => {}
-            SwarmEvent::ConnectionEstablished { peer_id, .. } if peer_id == remote => {
-                info!("connection to {} established", peer_id);
-                return true;
-            }
-            SwarmEvent::OutgoingConnectionError { peer_id, error } if peer_id == Some(remote) => {
-                debug!("connection error: {:?}", error);
-                return false;
-            }
-            SwarmEvent::Behaviour(Event::Ping(_)) => {}
-            SwarmEvent::NewListenAddr { address, .. } => {
-                info!("listening on {}", address);
-            }
-            ev => {
-                debug!("other event: {:?}", ev);
-            }
-        }
     }
 }
--- test/src/main.rs
+++ test/src/main.rs
@@ -2,26 +2,23 @@ use std::env;
 use std::path::Path;
 use tf_libp2p::{get_psk, Libp2pHost};
 // use tf_stellar::{Client, Network};
+use pretty_env_logger;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    pretty_env_logger::init();
     // let _ = Client::new("seed", Network::new_public())?;
 
     let args: Vec<String> = env::args().collect();
 
     let relay_addr = &args[1];
-    let relay_peer_id = &args[2];
 
     let psk = get_psk(&Path::new("."))?;
-    let host = Libp2pHost::new(None, psk).await?;
-    println!("host created");
+    let mut host = Libp2pHost::new(None, psk).await?;
 
-    // tokio::spawn(async move {
-    //     host.start(30334).await;
-    // });
+    host.connect_to_relay(relay_addr.to_string()).await?;
 
-    host.connect_to_relay(relay_addr.to_string(), &relay_peer_id)
-        .await?;
+    host.run().await.unwrap();
 
     Ok(())
 }
